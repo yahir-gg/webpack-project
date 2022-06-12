2522e0fc77d5bdf98f92f6b0f07f52eec2317179
Yahir Garcia <yahirgarcia1996@gmail.com>
2022-06-11 19:40:26 -0500

    webpack terminado

---
+++ src/utils/getData.js
@@ -0,0 +1,16 @@
+
+//Se lee desde las variables de entorno
+const API = process.env.API;
+
+const getData = async (id) => {
+  const apiURl = id ? `${API}${id}` : API;
+  try {
+    const response = await fetch(apiURl);
+    const data = await response.json();
+    return data.results[0];
+  } catch (error) {
+    console.log('Fetch Error', error);
+  };
+};
+
+export default getData;
